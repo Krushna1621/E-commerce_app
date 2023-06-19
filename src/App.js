5b8164b8066a7eedcd76f9d729d027fd22ee2b87
Krushna1621 <shindekrushna825@gmail.com>
2023-06-19 18:47:40 +0530

    task no 13 contact page added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import { useState } from 'react';
-import Footer from './components/Footer/Footer';
-import Header from './components/Header/Header';
-import CartProvider from './store/CartProvider';
-import { Routes, Route } from 'react-router-dom';
-import Store from './pages/Store';
-import Home from './pages/Home';
-import About from './pages/About';
+import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Footer from './components/Footer/Footer'
+import Header from './components/Header/Header'
+import CartProvider from './store/CartProvider'
+import Store from './pages/Store'
+import Home from './pages/Home'
+import About from './pages/About'
+import ContactUs from './pages/ContactUs'
 
 function App() {
   return (
     <CartProvider>
       <Header />
       <Routes>
-        <Route path="/home" element={<Home />} />
-        <Route path="/store" element={<Store />} />
-        <Route path="/about" element={<About />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/store' element={<Store />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<ContactUs />} />
       </Routes>
       <Footer />
     </CartProvider>
-  );
+  )
 }
 
-export default App;
+export default App
