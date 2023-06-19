cc1ec55d5737187355b7558c918dbfe394438781
Krushna1621 <shindekrushna825@gmail.com>
2023-06-19 19:24:25 +0530

    task 13

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { useState } from 'react'
-import { Routes, Route } from 'react-router-dom'
 import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header'
 import CartProvider from './store/CartProvider'
+import { Route, Routes } from 'react-router-dom';
 import Store from './pages/Store'
 import Home from './pages/Home'
 import About from './pages/About'
 import ContactUs from './pages/ContactUs'
+import EachProduct from './pages/EachProduct'
 
 function App() {
   return (
@@ -17,10 +18,11 @@ function App() {
         <Route path='/store' element={<Store />} />
         <Route path='/about' element={<About />} />
         <Route path='/contact' element={<ContactUs />} />
+        <Route path='/products/:productId' element={<EachProduct />} />
       </Routes>
       <Footer />
     </CartProvider>
-  )
+  );
 }
 
-export default App
+export default App;
